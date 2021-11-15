b74f68629c1ccd47b1a9cd604eddd37c36dfec4c
axelasa <asaaxel82@gmail.com>
2021-11-15 14:47:28 +0300

    Todo List First changes

---
+++ app/src/main/java/com/axel/todolist/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.axel.todolist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xFFffab91)
+val RedPink = Color(0xFFf48fb1)
+val BabyBlue = Color(0xFF81deea)
+val Violet = Color(0xFFcf94da)
+val LightGreen = Color(0xFFefed9b)
